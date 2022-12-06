8fa0722190ecd98b145e8cc1a6fd026c39178e1f
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2022-12-06 23:23:37 +0200

    add edit note view page

--- lib/widgets/note_item.dart
+++ lib/widgets/note_item.dart
@@ -1,55 +1,66 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:todo_hive/view/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
-      child: Container(
-        decoration: BoxDecoration(
-          color: const Color(0xffFFCC80),
-          borderRadius: BorderRadius.circular(16),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              ListTile(
-                textColor: Colors.black,
-                title: const Text(
-                  'Flutter Tips',
-                  style: TextStyle(fontSize: 23),
-                ),
-                subtitle: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    'This is first note body',
-                    style: TextStyle(color: Colors.black.withOpacity(.4)),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const EditNoteView(),
+          ),
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+        child: Container(
+          decoration: BoxDecoration(
+            color: const Color(0xffFFCC80),
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                ListTile(
+                  textColor: Colors.black,
+                  title: const Text(
+                    'Flutter Tips',
+                    style: TextStyle(fontSize: 23),
+                  ),
+                  subtitle: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'This is first note body',
+                      style: TextStyle(color: Colors.black.withOpacity(.4)),
+                    ),
+                  ),
+                  trailing: IconButton(
+                    icon: const Icon(FontAwesomeIcons.trash),
+                    color: Colors.black,
+                    onPressed: () {},
                   ),
                 ),
-                trailing: IconButton(
-                  icon: const Icon(FontAwesomeIcons.trash),
-                  color: Colors.black,
-                  onPressed: () {},
+                const SizedBox(
+                  height: 30,
                 ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Align(
-                alignment: Alignment.centerRight,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    '22 Aug',
-                    style: TextStyle(color: Colors.black.withOpacity(.4)),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      '22 Aug',
+                      style: TextStyle(color: Colors.black.withOpacity(.4)),
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
