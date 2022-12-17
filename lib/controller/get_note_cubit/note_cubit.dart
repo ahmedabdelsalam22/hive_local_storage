3719e9302bbb098ca0ae9e9ed6d89683ae1402a9
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2022-12-17 21:19:45 +0200

    create noteCubit and state

---
+++ lib/controller/get_note_cubit/note_cubit.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../models/note_model.dart';
+
+part 'note_state.dart';
+
+class NoteCubit extends Cubit<NoteState> {
+  NoteCubit() : super(NoteInitial());
+
+  static NoteCubit get(context) => BlocProvider.of(context);
+}
