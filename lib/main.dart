1b3d83386dc8cc93fce41bb09f41bbf2ed8c7a6b
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2022-12-17 15:44:55 +0200

    blocProvider

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:todo_hive/cubit/bloc_observer.dart';
-import 'package:todo_hive/cubit/notes_cubit.dart';
 import 'package:todo_hive/shared/app_theme.dart';
 import 'package:todo_hive/shared/constance.dart';
 import 'package:todo_hive/view/notes_view.dart';
@@ -13,8 +12,8 @@ void main() async {
   await Hive.initFlutter();
 
   Bloc.observer = SimpleBlocObserver();
-  await Hive.openBox<NoteModel>(AppConsts.kNotesBox);
   Hive.registerAdapter(NoteModelAdapter());
+  await Hive.openBox<NoteModel>(AppConsts.kNotesBox);
   runApp(const MyApp());
 }
 
@@ -24,15 +23,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (context) => AddNoteCubit()),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: lightTheme,
-        home: const NotesView(),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: lightTheme,
+      home: const NotesView(),
     );
   }
 }
--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -14,17 +14,20 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: BlocConsumer<AddNoteCubit, AddNoteState>(
-        listener: (context, state) {
-          if (state is AddNoteError) debugPrint("failed${state.msg}");
-          if (state is AddNoteSuccess) Navigator.pop(context);
-        },
-        builder: (context, state) {
-          return ModalProgressHUD(
-            inAsyncCall: state is AddNoteLoading ? true : false,
-            child: const AddNoteForm(),
-          );
-        },
+      child: BlocProvider(
+        create: (context) => AddNoteCubit(),
+        child: BlocConsumer<AddNoteCubit, AddNoteState>(
+          listener: (context, state) {
+            if (state is AddNoteError) debugPrint("failed${state.msg}");
+            if (state is AddNoteSuccess) Navigator.pop(context);
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+              inAsyncCall: state is AddNoteLoading ? true : false,
+              child: const AddNoteForm(),
+            );
+          },
+        ),
       ),
     );
   }
