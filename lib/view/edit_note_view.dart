1072338d6f127c2408c701463d4ac966f6502dd7
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2022-12-19 22:11:15 +0200

    some features to note_view_edit

--- lib/view/edit_note_view.dart
+++ lib/view/edit_note_view.dart
@@ -1,14 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:todo_hive/models/note_model.dart';
 
-import '../widgets/edit_note_view_body.dart';
+import '../widgets/custom_appbar.dart';
+import '../widgets/custom_text_field.dart';
 
 class EditNoteView extends StatelessWidget {
-  const EditNoteView({Key? key}) : super(key: key);
+  const EditNoteView({Key? key, required this.noteModel}) : super(key: key);
+  final NoteModel noteModel;
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: EditNoteViewBody(),
-    );
+    return Scaffold(
+        body: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 50,
+          ),
+          CustomAppBar(
+            title: 'Edit',
+            icon: Icons.check,
+            onPressedIcon: () {},
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          CustomTextField(
+            hint: noteModel.title,
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          CustomTextField(
+            hint: noteModel.subTitle,
+            maxLine: 5,
+          ),
+        ],
+      ),
+    ));
   }
 }
